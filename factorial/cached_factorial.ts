8b782e96d2250f8934d2a358945024102f0d16b9
Roland Chelwing <jolleee@hotmail.com>
2022-05-19 10:09:04 +0200

    Better structure

---
+++ factorial/cached_factorial.ts
@@ -0,0 +1,8 @@
+{
+  const memo = {};
+  const factorial = (n: number): number => {
+    if (n == 0 || n == 1) return 1;
+    if (!(n in memo)) memo[n] = n * factorial(n - 1);
+    return memo[n];
+  };
+}
---
+++ factorial/regular_factorial.ts
@@ -0,0 +1,6 @@
+{
+  const factorial = (n: number): number => {
+    if (n == 0 || n == 1) return 1;
+    return n * factorial(n - 1);
+  };
+}
